f22d03c62b9a03d0ceb70c663a79ae37e0b30c4c
chyojn <chyojn@gmail.com>
2014-03-11 14:49:51 +0800

    install.sh: run vim to install bundle.

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+#clone vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
+#set .vimrc
 ln -s ~/dotvim/vimrc ~/.vimrc
 
+#run vim and install bundle
+vim -c "BundleInstall"
